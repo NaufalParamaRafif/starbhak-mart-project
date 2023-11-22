8411dc98ce4448c6329d357a1924a248db501387
NaufalParamaRafif <profesoraiueo@gmail.com>
2023-11-22 11:29:39 +0700

    add quality increment

--- javascript_query.js
+++ javascript_query.js
@@ -1,7 +1,36 @@
 $(document).ready(function(){
-  $(".food-menu-container button").click(function(){
-    $name_of_food = $(".food-menu-container button h5").text();
-    $price = $(".food-menu-container button h6").text();
-    $( "<p id='test'>Food name: "+$name_of_food+", Price: "+$price+"</p>" ).appendTo(".item-in-troll-container" );
+  $(".menu").click(function(){
+    
+    nameFood = $("h5", this).text();
+    unitPrice = $("h6", this).text();
+
+    if ($("."+nameFood+"").length) {
+      quantity = parseInt($("."+nameFood+" .quantity span").text());
+      totalPrice = parseFloat(convertRupiahToInt($("."+nameFood+" .total-price span").text()));
+      unitPrice = parseFloat(convertRupiahToInt($("."+nameFood+".unit-price span").text()));
+      quantity++;
+      totalPrice += unitPrice;
+
+      $("."+nameFood+" .quantity span").text(quantity);
+      $("."+nameFood+" .total-price span").text(totalPrice);
+      
+    } else {
+      newElement = '<div class="item-in-troll '+nameFood+'">' +
+          '<p class="name-of-food">'+nameFood+'</p>' +
+          '<p class="total-price right"><span class="right">'+unitPrice+'</span></p>' +
+          '<div class="item-in-troll-image">' +
+            '<img src="images/trash.svg" alt="">' +
+          '</div>' +
+          '<p class="unit-price">Unit price <span class="right" id="unit-price">'+unitPrice+'</span></p>' +
+          '<p class="quantity">Quantity: <span class="right">1</span></p>' +
+        '</div>';
+      $(newElement).appendTo(".item-in-troll-container" );
+    }
   });
 });
+
+function convertRupiahToInt(hargaString){
+  hargadislide = hargaString.slice(4);
+  hargaInt = parseInt(hargadislide);
+  return hargaInt;
+}
