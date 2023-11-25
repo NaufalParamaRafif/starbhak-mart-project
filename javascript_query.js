9324d7846417644e4c4233b45adacf366858a9c4
NaufalParamaRafif <profesoraiueo@gmail.com>
2023-11-25 16:48:16 +0700

    hampir kelar

--- javascript_query.js
+++ javascript_query.js
@@ -16,16 +16,16 @@ function MenuClicked(){
       totalPrice = quantity * unitPrice;
 
       $("."+nameFood+" .quantity span").text(quantity);
-      $("."+nameFood+" .total-price span").text(totalPrice);
+      $("."+nameFood+" .total-price span").text(convertFloatToRupiah(totalPrice));
       
     } else {
       newElement = '<div class="item-in-troll '+nameFood+'">' +
           '<p class="name-of-food">'+nameFood+'</p>' +
-          '<p class="total-price right"><span class="right">'+unitPrice+'</span></p>' +
+          '<p class="total-price right"><span class="right">'+convertFloatToRupiah(unitPrice)+'</span></p>' +
           '<div class="item-in-troll-image">' +
             '<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>' +
           '</div>' +
-          '<p class="unit-price">Unit price <span class="right" id="unit-price">'+unitPrice+'</span></p>' +
+          '<p class="unit-price">Unit price <span class="right" id="unit-price">'+convertFloatToRupiah(unitPrice)+'</span></p>' +
           '<p class="quantity">Quantity: <span class="right">1</span></p>' +
         '</div>';
       $(newElement).appendTo(".item-in-troll-container" );
@@ -41,10 +41,10 @@ function MenuClicked(){
 function deleteItem(){
   $(".item-in-troll-container").on('click', '.item-in-troll-image svg', function(){
     nameFood = $(this).parent().parent().attr('class').split(' ')[1];
-    unitPrice = parseFloat($('.'+nameFood+' .unit-price span').text());
+    unitPrice = convertRupiahToFloat($('.'+nameFood+' .unit-price span').text());
 
     quantity = parseInt($('.'+nameFood+' .quantity span').text());
-    totalPrice = parseFloat($('.'+nameFood+' .total-price span').text());
+    totalPrice = convertRupiahToFloat($('.'+nameFood+' .total-price span').text());
     if (quantity > 1) {
       quantity--;
       totalPrice -= unitPrice;
@@ -71,15 +71,28 @@ function decreaseTotalAmount(currentTotalAmount, currentTax, unitPrice){
   tax = currentTax - (unitPrice * (0.1));
   $('.tax').text(convertFloatToRupiah(tax));
 
-  totalAmount = currentTotalAmount - (unitPrice + unitPrice * 0.1);
+  totalAmount = currentTotalAmount - (unitPrice + (unitPrice * 0.1));
   $('.total-amount').text(convertFloatToRupiah(totalAmount));
 }
-function convertRupiahToFloat(hargaString){
-  hargadislide = hargaString.slice(4);
-  hargaFloat = parseFloat(hargadislide);
-  return hargaFloat;
+// function convertRupiahToFloat(hargaString){
+//   hargadislide = hargaString.slice(4);
+//   hargaFloat = parseFloat(hargadislide);
+//   return hargaFloat;
+// }
+const convertRupiahToFloat = (rupiahString) => {
+  const cleanedString = rupiahString.replace(/[^\d,-]/g, ''); // Menghilangkan karakter selain angka, koma, dan minus
+  const floatValue = parseFloat(cleanedString.replace(',', '.')); // Mengganti koma dengan titik dan mengonversi ke float
+  return isNaN(floatValue) ? 0 : floatValue; // Memastikan bahwa hasilnya adalah float atau 0 jika tidak valid
+};
+
+const convertFloatToRupiah = (number)=>{
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR"
+  }).format(number);
 }
-function convertFloatToRupiah(number){
-  valueRupiah = "Rp. " + number
-  return valueRupiah;
-}
+
+// function convertFloatToRupiah(number){
+//   valueRupiah = "Rp. " + number
+//   return valueRupiah;
+// }
